d9b2e0c6e4696853dd98f8ec87cba1a1bbad8385
arvinkumar <45001739+arvincolombes@users.noreply.github.com>
2025-02-04 20:55:23 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,6 +6,7 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-053b0d53c279acc90" # replace this
   instance_type_value = "t2.micro"
+  sg		= "seucrity"
   subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
   tag_name = "arvinkumar"
 }
