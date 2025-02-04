8c5009d4a81f8452c489d6e6ec00d2bba7e50a45
arvinkumar <45001739+arvincolombes@users.noreply.github.com>
2025-02-04 15:35:14 +0530

    Update main.tf
    add key values

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
+    key_name = data.aws_key_pair.oregon.key
+}
+tag = {
+	Name = "${var.tag_name}instance
 }
