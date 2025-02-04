a544b330149eccb47c61cc36a9b3a11a2dd42bc5
arvinkumar <45001739+arvincolombes@users.noreply.github.com>
2025-02-04 21:32:05 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "example" {
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
     key_name = data.aws_key_pair.oregon.key
+    vpc_security_group_ids = var.security_group_id
 }
 tag = {
 	Name = "${var.tag_name}instance
