b2c8e1cbebfb948dabefd3384d76881f813bffa5
arvinkumar <45001739+arvincolombes@users.noreply.github.com>
2025-02-05 21:25:58 +0530

    Update variable.tf

--- day10/module/ec2/variable.tf
+++ day10/module/ec2/variable.tf
@@ -12,3 +12,19 @@ variable "ec2-cout" {
 description "number of ec2 instance to create" 
 default 1 
 }
+sg_ingress = [ 
+{
+port     = "80" 
+protocal = "tcp" 
+cidrs    = ["0.0.0.0/0"] 
+},
+{
+port     = "22" 
+protocal = "tcp" 
+cidrs    = ["172.31.0.0/16"] 
+},
+{
+port     = "22" 
+protocal = "tcp" 
+cidrs    = [""0.0.0.0/0"] 
+}
