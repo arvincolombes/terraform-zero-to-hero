865b07f80595c6d1eb2d1307682be807f3709db8
arvinkumar <45001739+arvincolombes@users.noreply.github.com>
2025-02-05 21:47:05 +0530

    Update main.tf

--- day10/module/ec2/main.tf
+++ day10/module/ec2/main.tf
@@ -1,24 +1,42 @@
-module "demo-web" 
-source = "./modules/ec2" 
-ami "ami-09d3b3274b6c5d4aa" 
-subnet id="subnet-0b2062968bcc711d2" 
-key_name = "bcreddydevopsluseast1" 
-vpc_id = "vpc-8f021d41550012265" 
-sg_ingress = [ 
-{
-port     = "80" 
-protocal = "tcp" 
-cidrs    = ["0.0.0.0/0"] 
-},
-{
-port     = "22" 
-protocal = "tcp" 
-cidrs    = ["172.31.0.0/16"] 
-},
-{
-port     = "22" 
-protocal = "tcp" 
-cidrs    = [""0.0.0.0/0"] 
+resource "aws_instance" "main" {
+count var.ec2-cout 
+instance_type var.instance_type 
+ami var.ami 
+subnet id var.subnet id 
+vpc_security_group_ids = [aws_security_group.sgs.id] 
+key_name var.key_name 
+tags { 
+"Name" "demomodule"
+ }
 }
-
-
+resource "aws_security_group" "sgs" { 
+name = "ec2-for-sg-instance" 
+description = "Allow TLS inbound traffic" 
+vpc_id = var.vpc_id 
+ingress { 
+description = "TLS from VPC 
+from_port = var.sg_ingress[0].port 
+to_port = var.sg_ingress[0].port 
+protocol - var.sg_ingress[0].protocal 
+cidr_blocks var.sg_ingress[0].cidrs
+}
+dynamic "Ingress" { 
+for_each var.sg_ingress 
+content ( 
+description = "TLS from VPC" 
+from_port   = ingress.value.port 
+to port     = ingress.value.port 
+protocol    = ingress.value.protocal 
+cidr_blocks =  ingress.value.cidrs 
+  } 
+}
+egress [ 
+from_port        = 0 
+to_port          = 0
+protocol         ="-1"
+cidr blocks      = ["0.0.0.0/0"] 
+ipv6_cidr_blocks = ["::/8"] 
+} 
+tags = { 
+Name "allow_tls" 
+}
